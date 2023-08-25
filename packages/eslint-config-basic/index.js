35d3741c0cc59ada480181b5dafc28dec9ca5cde
donny <donny526@outlook.com>
2023-08-25 16:14:11 +0800

    chore: ignore vitepress cache files

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -37,7 +37,7 @@ module.exports = {
     '!.vitepress',
     '!.vscode',
     // force exclude
-    '.vitepress/cache',
+    '**/.vitepress/cache',
   ],
   plugins: ['html', 'unicorn', 'coderwyd', 'no-only-tests', 'unused-imports'],
   settings: {
