c266aa470e220efea6b9db33764ac534422258a4
donny <donny526@outlook.com>
2023-12-29 12:20:12 +0800

    fix: lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,17 @@
-// eslint-disable-next-line antfu/no-import-dist
-import { defineConfig } from './dist/index.js'
+import 'tsx'
+import { createRequire } from 'node:module'
 
+// eslint-disable-next-line antfu/no-import-dist
+import { defineConfig as defineConfig2 } from './dist/index.js'
+
+const require = createRequire(import.meta.url)
+
+const { defineConfig: defineConfig1 } = require('./src/index.ts')
+
+const useBuild = true
+
+/** @type {import('./src/index.ts').defineConfig} */
+const defineConfig = useBuild ? defineConfig2 : defineConfig1
 export default defineConfig(
   {
     vue: true,
