bd5d34ea67983f91dcbe4ae243e088c6871c1cf5
donny <donny526@outlook.com>
2024-03-05 14:00:06 +0800

    fix: use `VSCODE_CWD` to autodetect if in editor

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,10 @@ import process from 'node:process'
 import { isPackageExists } from 'local-pkg'
 
 export const isInEditor = !!(
-  (process.env.VSCODE_PID || process.env.JETBRAINS_IDE || process.env.VIM) &&
+  (process.env.VSCODE_PID ||
+    process.env.VSCODE_CWD ||
+    process.env.JETBRAINS_IDE ||
+    process.env.VIM) &&
   !process.env.CI
 )
 export const hasTypeScript = isPackageExists('typescript')
