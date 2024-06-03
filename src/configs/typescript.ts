484deea89ff9332a8c6dffb60228ca9ac1644e15
donny <donny526@outlook.com>
2024-06-03 10:12:51 +0800

    feat(typescript): enable `strict-boolean-expressions` rule

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -51,6 +51,7 @@ export async function typescript(
     'ts/no-unsafe-return': 'error',
     'ts/restrict-plus-operands': 'error',
     'ts/restrict-template-expressions': 'error',
+    'ts/strict-boolean-expressions': 'error',
     'ts/unbound-method': 'error',
   }
 
