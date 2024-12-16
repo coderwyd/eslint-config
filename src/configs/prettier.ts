6cc70d39549f70ad156dfbe24aae03e958b3b270
donny <donny526@outlook.com>
2024-12-16 11:41:25 +0800

    feat: remove `eslint-plugin-prettier` plugin

--- src/configs/prettier.ts
+++ src/configs/prettier.ts
@@ -1,23 +1,16 @@
 import prettierRules from 'eslint-config-prettier'
-import { interopDefault } from '../shared'
 import type { TypedFlatConfigItem } from '../types'
 
 const { rules: eslintRules } = prettierRules
 
-export async function prettier(): Promise<TypedFlatConfigItem[]> {
-  const pluginPrettier = await interopDefault(import('eslint-plugin-prettier'))
-
+export function prettier(): TypedFlatConfigItem[] {
   return [
     {
       name: 'coderwyd/prettier/rules',
-      plugins: {
-        prettier: pluginPrettier,
-      },
       rules: {
         ...eslintRules,
         'arrow-body-style': 'off',
         'prefer-arrow-callback': 'off',
-        'prettier/prettier': 'warn',
       },
     },
   ]
