d846e621293f0ed92c23e10cbdfb0bbf947568c8
donny <donny526@outlook.com>
2025-04-24 15:39:55 +0800

    fix: update import groups in perfectionist config for better organization

--- src/configs/perfectionist.ts
+++ src/configs/perfectionist.ts
@@ -22,16 +22,41 @@ export function perfectionist(): TypedFlatConfigItem[] {
           'warn',
           {
             groups: [
-              'builtin',
-              'external',
-              'type',
-              ['internal', 'internal-type'],
-              ['parent', 'sibling', 'index'],
-              ['parent-type', 'sibling-type', 'index-type'],
-              'object',
-              'side-effect',
+              // Side effect style imports (e.g. 'normalize.css')
               'side-effect-style',
-              'style',
+              // Styles (e.g. *.{css,scss,less})
+              'value-style',
+              // Node.js built-in modules. (e.g. fs, path)
+              'value-builtin',
+              // External modules installed in the project (e.g. vue, lodash)
+              'value-external',
+              // Node.js subpath imports (e.g. fs/promises)
+              'value-subpath',
+              // Internal modules (e.g. @/utils, @/components)
+              'value-internal',
+              // Modules from parent directory (e.g. ../utils)
+              'value-parent',
+              // Modules from the same directory (e.g. ./utils)
+              'value-sibling',
+              // Main file from the current directory (e.g. ./index)
+              'value-index',
+              // TypeScript import-equals imports (e.g. import log = console.log)
+              'ts-equals-import',
+              // Side effect imports (e.g. import 'babel-polyfill')
+              'side-effect',
+              /**
+               * Type import at the end
+               */
+              'type-builtin',
+              'type-external',
+              'type-subpath',
+              'type-internal',
+              'type-parent',
+              'type-sibling',
+              'type-index',
+              /**
+               * Imports that don’t fit into any other group
+               */
               'unknown',
             ],
             internalPattern: ['^[~@#]/.*'],
